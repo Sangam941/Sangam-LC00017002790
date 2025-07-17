d15ba5a71448a631d4cd28e4a42d1ce285bf9a44
Sangam Singh Dhami <sangamdhami663@gmail.com>
2025-07-17 13:44:02 +0600

    user api

--- Django/users/urls.py
+++ Django/users/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('create/', views.create, name="create-page"),
     path('user-create/', views.UsersCreateList.as_view(), name="user-list-create"),
+    path('user-create/<int:pk>', views.UserUpdateDelete.as_view(), name="user-list-create"),
 ]
--- Django/users/views.py
+++ Django/users/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from rest_framework.response import Response
 from rest_framework import generics
 from .models import Users
 from .serializers import UserSerializer
@@ -11,3 +12,12 @@ def create(request):
 class UsersCreateList(generics.ListCreateAPIView):
     queryset = Users.objects.all()
     serializer_class = UserSerializer
+
+    def delete(self, request, *args, **kwrgs):
+        Users.objects.all().delete()
+        return Response(status = status.HTTP_204_NO_CONTENT)
+
+class UserUpdateDelete(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Users.objects.all()
+    serializer_class = UserSerializer
+    lookup_field = 'pk'
